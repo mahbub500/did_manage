b2af1d72e8c47f14499d60e5c35bec51770a1f8e
root <mahbubmr500@gmail.com>
2023-12-20 16:47:04 +0600

    misc

--- views/did.php
+++ views/did.php
@@ -26,10 +26,10 @@ use Codexpert\WpDid\Helper;
 	<div class="row">
 		<div class="col-md-3">
 			<div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
-			  <a class="nav-link active" id="did" data-toggle="pill" href="#add_did" role="tab" aria-controls="add_did" aria-selected="true">Add DID</a>
-			  <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false">Add NID</a>
-			  <a class="nav-link" id="v-pills-messages-tab" data-toggle="pill" href="#v-pills-messages" role="tab" aria-controls="v-pills-messages" aria-selected="false">NId List</a>
-			  <a class="nav-link" id="v-pills-settings-tab" data-toggle="pill" href="#v-pills-settings" role="tab" aria-controls="v-pills-settings" aria-selected="false">Did List</a>
+			  <a class="nav-link active" id="did" data-toggle="pill" href="#add_did" role="tab" aria-controls="add_did" aria-selected="true">DID</a>
+			  <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#nid-add" role="tab" aria-controls="nid-add" aria-selected="false">NID</a>
+			  <a class="nav-link" id="v-pills-messages-tab" data-toggle="pill" href="#nid-list" role="tab" aria-controls="nid-list" aria-selected="false">NId List</a>
+			  <a class="nav-link" id="v-pills-settings-tab" data-toggle="pill" href="#did-list" role="tab" aria-controls="did-list" aria-selected="false">Did List</a>
 			</div>
 		</div>	
 		<div class="col-md-7">
@@ -37,15 +37,15 @@ use Codexpert\WpDid\Helper;
 			  <div class="tab-pane fade show active" id="add_did" role="tabpanel" aria-labelledby="did">
 			  	<?php echo Helper::get_template( 'add_did' ) ?>			  	
 			  </div>
-			  <div class="tab-pane fade" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab">
+			  <div class="tab-pane fade" id="nid-add" role="tabpanel" aria-labelledby="v-pills-profile-tab">
 			  	<h1>NID</h1>
 			  	<?php echo Helper::get_template( 'add_nid' ) ?>
 			  </div>
-				<div class="tab-pane fade" id="v-pills-messages" role="tabpanel" aria-labelledby="v-pills-messages-tab">
+				<div class="tab-pane fade" id="nid-list" role="tabpanel" aria-labelledby="v-pills-messages-tab">
 					<h1>List</h1>
 			  		<?php echo Helper::get_template( 'nid_list' ) ?>			  	
 				</div>
-				<div class="tab-pane fade" id="v-pills-settings" role="tabpanel" aria-labelledby="v-pills-settings-tab">
+				<div class="tab-pane fade" id="did-list" role="tabpanel" aria-labelledby="v-pills-settings-tab">
 					<?php echo Helper::get_template( 'did_list' ) ?>
 				</div>
 			</div>
