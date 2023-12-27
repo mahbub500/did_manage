eb25bc9f2a0e91f621f7914ecaa4e0dad7a98b99
root <mahbubmr500@gmail.com>
2023-12-27 12:00:53 +0600

    misc

---
+++ vendor/composer/installed.php
@@ -0,0 +1,56 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'd8c8c8899737c51c3f0ea87e7de133d583c11964',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'd8c8c8899737c51c3f0ea87e7de133d583c11964',
+            'dev_requirement' => false,
+        ),
+        'mahbub/plugin' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../mahbub/plugin',
+            'aliases' => array(
+                0 => '9999999-dev',
+            ),
+            'reference' => 'a6c822136119ff67bcb4a0717e7adfddf4fa3bca',
+            'dev_requirement' => false,
+        ),
+        'pluggable/marketing' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../pluggable/marketing',
+            'aliases' => array(
+                0 => '9999999-dev',
+            ),
+            'reference' => '0a114f7a318bdf8d3b0bddef0174cbd831c2ea6a',
+            'dev_requirement' => false,
+        ),
+        'pluggable/plugin' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../pluggable/plugin',
+            'aliases' => array(
+                0 => '9999999-dev',
+            ),
+            'reference' => 'f1868e82196de6c6ed1e21773665f15e8af77c4e',
+            'dev_requirement' => false,
+        ),
+    ),
+);
